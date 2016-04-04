61e564754468b04c87e46b66f7cc56a75ac2ad16
Vas Richard <kyler50vasrich@gmail.com>
2016-04-04 15:28:02 +0200

    work finished

---
+++ Phonebook/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phonebook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            setCombo();
+        }
+
+        public void setCombo()
+        {
+            Box1.Items.Add(Type.Home.ToString());
+            Box1.Items.Add(Type.Mobile.ToString());
+            Box1.Items.Add(Type.Workplace.ToString());
+        }
+
+        private void save_button(object sender, EventArgs e)
+        {
+            Directory.persons.Add(new Person(textBox1.Text, textBox2.Text, Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text), Box1.Text));
+        }
+
+        private void list_button(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < Directory.persons.Count; i++)
+            {
+                listBox1.Items.Add(Directory.persons[i].ToString());
+            }
+        }
+    }
+}
---
+++ Phonebook/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    struct Person
+    {
+        public Person(string name, string address, int age, int mobile, string type)
+        {
+            this.name = name;
+            this.address = address;
+            this.age = age;
+            this.mobile = mobile;
+            this.type = type;
+        }
+
+        string name;
+        string address;
+        int age;
+        int mobile;
+        string type;
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Address: {1}, Age: {2}, Mobile: {3}, Type: {4}", name, age, address, mobile, type.ToString());
+        }
+    }
+    class Directory
+    {
+        public static List<Person> persons = new List<Person>(50);
+    }
+    enum Type
+    {
+        Home, Workplace, Mobile
+    }
+}
